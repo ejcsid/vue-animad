df5633753255b000e2f9ab73b3acee442bb14b3d
Admin <ejcsid@gmail.com>
2018-12-21 12:56:44 +0100

    Initial Navigation Bar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,53 @@
 <template>
   <v-app>
+    <v-navigation-drawer app permanent>
+      <v-toolbar flat>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title class="title">
+              <v-icon>reorder</v-icon>
+              Animad-App
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+
+      <v-divider></v-divider>
+
+      <v-list dense class="pt-0">
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title"
+          @click=""
+        >
+          <v-list-tile-action>
+            <v-icon> {{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title> {{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class="font-weight-light">Placeholder</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
+      <!--v-spacer></v-spacer>
       <v-btn
         flat
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
       >
         <span class="mr-2">Latest Release</span>
-      </v-btn>
+      </v-btn-->
     </v-toolbar>
 
     <v-content>
       <HelloWorld/>
     </v-content>
+    <v-footer app></v-footer>
   </v-app>
 </template>
 
@@ -31,7 +61,13 @@ export default {
   },
   data () {
     return {
-      //
+      items: [
+        {title: "Animals", icon: "pets"},
+        {title: "Keepers", icon: "face"},
+        {title: "Enclosures", icon: "account_balance"}
+
+      ]
+
     }
   }
 }
