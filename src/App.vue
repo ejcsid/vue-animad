93024ba93e52fc3baea91879374e3a7eac97df67
Admin <ejcsid@gmail.com>
2018-12-21 13:05:35 +0100

    Logout Link - navigation to an external link...

--- src/App.vue
+++ src/App.vue
@@ -31,17 +31,17 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span class="font-weight-light">Placeholder</span>
-      </v-toolbar-title>
-      <!--v-spacer></v-spacer>
+      <!--v-toolbar-title class="headline text-uppercase">
+      </v-toolbar-title-->
+      <v-spacer></v-spacer>
       <v-btn
         flat
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
       >
-        <span class="mr-2">Latest Release</span>
-      </v-btn-->
+        <span class="mr-2">Logout</span>
+        <v-icon>input</v-icon>
+      </v-btn>
     </v-toolbar>
 
     <v-content>
