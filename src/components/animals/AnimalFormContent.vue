645eb8f9ec16a2012eb59da48e0da503ea735fce
Schmid Silke <schmid_silke@web.de>
2019-01-08 17:14:01 +0100

    New Component KeeperDialog

--- src/components/animals/AnimalFormContent.vue
+++ src/components/animals/AnimalFormContent.vue
@@ -1,102 +1,63 @@
 <template>
   <v-container>
+    <v-text-field
+      label="Name"
+      v-model="animal.name"
+      clearable
+      required
+      :rules="nameRules"
+    />
+    <v-select
+      label="Type"
+      :items="types"
+      v-model="animal.type"
+    />
+    <v-menu
+      ref="menu"
+      :close-on-content-click="false"
+      :nudge-right="40"
+      lazy
+      transition="scale-transition"
+      offset-y
+      full-width
+      min-width="290px"
+    >
       <v-text-field
-        label="Name"
-        v-model="animal.name"
-        clearable
-        required
-        :rules="nameRules"
+        slot="activator"
+        v-model="animal.birthday"
+        label="Birthday"
+        prepend-icon="event"
       />
-      <v-select
-        label="Type"
-        :items="types"
-        v-model="animal.type"
+      <v-date-picker
+        ref="picker"
+        v-model="animal.birthday"
+        no-title
+        scrollable
+        locale="DE"
+        :max="new Date().toISOString().substr(0, 10)"
+        @change="doSaveBirthday"
       />
-      <v-menu
-        ref="menu"
-        :close-on-content-click="false"
-        :nudge-right="40"
-        lazy
-        transition="scale-transition"
-        offset-y
-        full-width
-        min-width="290px"
-      >
-        <v-text-field
-          slot="activator"
-          v-model="animal.birthday"
-          label="Birthday"
-          prepend-icon="event"
-        />
-        <v-date-picker
-          ref="picker"
-          v-model="animal.birthday"
-          no-title
-          scrollable
-          locale="DE"
-          :max="new Date().toISOString().substr(0, 10)"
-          @change="doSaveBirthday"
-        />
-      </v-menu>
-      <v-select
-        label="Gender"
-        :items="gender"
-        v-model="animal.gender"
-      />
-      <v-text-field
-        label="Weight"
-        v-model="animal.weight"
-        mask="####"
-        required
-        :rules="[rules.required]"
-        suffix="kg"
-      />
-      <v-checkbox
-        label="Alive"
-        v-model="animal.alive"
-      />
+    </v-menu>
+    <v-select
+      label="Gender"
+      :items="gender"
+      v-model="animal.gender"
+    />
+    <v-text-field
+      label="Weight"
+      v-model="animal.weight"
+      mask="####"
+      required
+      :rules="[rules.required]"
+      suffix="kg"
+    />
+    <v-checkbox
+      label="Alive"
+      v-model="animal.alive"
+    />
 
     <v-form ref="dialog" lazy-validation>
-      <v-layout wrap>
-        <v-text-field
-          label="Search Keeper">
-        </v-text-field>
-        <v-dialog v-model="dialog" persistent max-width="600px">
-          <v-btn slot="activator" icon >
-            <v-icon>add_circle</v-icon>
-          </v-btn>
-          <v-card>
-            <v-card-title>
-              <span class="headline">Add Keeper</span>
-            </v-card-title>
-            <v-card-text>
-              <v-container grid-list-md>
-                <v-layout wrap>
-                  <v-flex xs12 sm6 md4>
-                    <v-text-field label="First name*" v-model="createdKeeper.firstname" required :rules="nameRules"></v-text-field>
-                  </v-flex>
-                  <v-flex xs12 sm6 md4>
-                    <v-text-field
-                      label="Last name*"
-                      hint="Keeper's lastname mandatory with min. 2 letters"
-                      persistent-hint
-                      v-model="createdKeeper.lastname"
-                      required
-                      :rules="nameRules"
-                    ></v-text-field>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-              <small>*indicates required field</small>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
-              <v-btn color="blue darken-1" flat @click="createKeeper()">Save</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-layout>
+      <KeeperDialog v-bind:nameRules='nameRules' v-bind:validated='validated' v-on:doCreate='doCreateKeeper($event)' v-on:doOpen='doOpenKeeper'/>
     </v-form>
     <v-form>
       <v-flex xs12>
@@ -122,7 +83,11 @@
 </template>
 
 <script>
+  import KeeperDialog from '../keepers/KeepersDialog'
   export default {
+    components: {
+      KeeperDialog
+    },
     props: [
       'nameRules',
       'rules',
@@ -134,25 +99,29 @@
       return {
         keepers: [],
         createdKeeper: {
-          firstname: "",
-          lastname: ""
+          firstname: "Create",
+          lastname: "Keeper"
         },
-        dialog: false
+        validated: false
       }
     },
     methods: {
       doSaveBirthday: function(date) {
         this.$refs.menu.save(date)
       },
-      createKeeper() {
+      doCreateKeeper(event) {
         if (this.$refs.dialog.validate()) {
-          this.keepers.push(this.createdKeeper)
-          this.createdKeeper = { firstname: "", lastname: ""}
-          this.dialog = false
-          this.saveSuccess = true
+          this.keepers.push(event)
+          this.validated = true
         }
+        else {
+          this.validated = false
+        }
+      },
+      doOpenKeeper() {
+        this.validated = false
       }
-    }
+    },
   }
 </script>
 
---
+++ src/components/keepers/KeepersDialog.vue
@@ -0,0 +1,82 @@
+<template>
+<v-layout wrap>
+    <v-text-field
+      label="Search Keeper">
+    </v-text-field>
+    <v-dialog v-model="dialogOpen" persistent max-width="600px">
+      <v-btn slot="activator" @click="doOpen" icon >
+        <v-icon>add_circle</v-icon>
+      </v-btn>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Add Keeper</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="First name*" v-model="createdKeeper.firstname" required :rules="nameRules"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field
+                  label="Last name*"
+                  hint="Keeper's lastname mandatory with min. 2 letters"
+                  persistent-hint
+                  v-model="createdKeeper.lastname"
+                  required
+                  :rules="nameRules"
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click="doCancel()">Close</v-btn>
+          <v-btn color="blue darken-1" flat @click="doCreate()">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</v-layout>
+</template>
+
+<script>
+  export default {
+    props: [
+      'nameRules',
+      'dialog',
+      'validated'
+    ],
+    data () {
+      return {
+        createdKeeper: {
+          firstname: "",
+          lastname: ""
+        },
+        dialogOpen: false,
+      }
+    },
+    methods: {
+      doCreate() {
+        this.$emit('doCreate', this.createdKeeper)
+        this.createdKeeper = { firstname: "", lastname: ""}
+      },
+      doCancel() {
+        this.dialogOpen = false
+        this.createdKeeper = { firstname: "", lastname: ""}
+      },
+      doOpen() {
+        this.$emit('doOpen')
+      }
+    },
+    watch: {
+      validated: function(val) {
+        if (val) this.dialogOpen = false
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
