1f2e6309207fd41b590d45eebcbaa1d9adb683ba
Schmid Silke <schmid_silke@web.de>
2019-01-08 18:01:18 +0100

    Komponente BaseListHeader und filtern der Liste

--- src/components/animals/AnimalOverview.vue
+++ src/components/animals/AnimalOverview.vue
@@ -6,35 +6,14 @@
       column
     >
       <BaseHeader icon='pets' title='Overview Animals'/>
+
       <v-container grid-list-md text-xs-center>
-          <v-layout row wrap>
-          <v-flex xs8>
-            <v-text-field id="searchText"
-              regular
-              placeholder="Search"
-              prepend-icon="search"
-              clearable
-              @input= "doSearch"
-              :success-messages=resultsText
-              v-model="searchText"
-            >
-          </v-text-field>
-          </v-flex>
-          <v-flex xs4>
-            <v-btn @click="doNew()">
-              New
-              <v-icon>add_circle</v-icon>
-            </v-btn>
-            <v-btn @click="doDelete()">
-              Delete
-              <v-icon>delete</v-icon>
-            </v-btn>
-          </v-flex>
-        </v-layout>
+        <BaseListHeader routerNew='animals-create' v-bind:resultsText='resultsText' v-on:doFilter='doSearch($event)'/>
+
         <v-flex xs12>
          <v-list two-line>
            <v-list-tile
-             v-for="item in items"
+             v-for="item in filteredItems"
            >
            <v-list-tile-avatar>
              <v-icon>pets</v-icon>
@@ -74,16 +53,17 @@
 
 <script>
   import BaseHeader from '../commons/BaseHeader'
+  import BaseListHeader from '../commons/BaseListHeader'
+
   export default {
     components: {
       BaseHeader,
+      BaseListHeader
     },
     data () {
       return {
-        search: "abc",
         searchCount: 0,
-        resultsText: "",
-        searchText: null,
+        searchText: "",
         items: [
           { name: "Ben", type: "Elefant", birthday: "2011-01-01", gender: "male", weight: 1234, alive: true},
           { name: "Berta", type: "Giraffe", birthday: "2016-03-02", gender: "female", weight: 721, alive: false}
@@ -91,16 +71,24 @@
         dialog: false
       }
     },
-    methods: {
-      doSearch: function() {
-        console.log("doSearch")
-        // TODO filter list/table rows and show correct results text
+    computed: {
+      filteredItems: function() {
+        console.log(this.searchText)
 
-        // searchText is null if clear-Button in search-Filed was clicked
-        this.searchText === null ? this.searchCount = 0 :
-          this.searchCount < 3 ? this.searchCount++ : this.searchCount = 0
-        this.searchText === null ? this.resultsText = "" :
-          this.searchCount === 0 ? this.resultsText = "No Data found" : this.resultsText = this.searchCount+" rows found"
+        return this.items.filter(item =>  item.name.toLowerCase().includes(this.searchText))
+      },
+      resultsText: function() {
+        if (this.filteredItems.length === 0) {
+          return "No Data found."
+        }
+        else {
+          return this.filteredItems.length+" rows found."
+        }
+      }
+    },
+    methods: {
+      doSearch: function(searchText) {
+        this.searchText = searchText === null ? "" : searchText
       },
       doNew: function() {
         this.$router.push({name: 'animals-create'})
---
+++ src/components/commons/BaseListHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs8>
+      <v-text-field id="searchText"
+        regular
+        placeholder="Search"
+        prepend-icon="search"
+        clearable
+        @input= "doFilter"
+        :success-messages=resultsText
+        v-model="searchText"
+      >
+    </v-text-field>
+    </v-flex>
+    <v-flex xs4>
+      <v-btn @click="doNew">
+        New
+        <v-icon>add_circle</v-icon>
+      </v-btn>
+      <v-btn @click="doDelete">
+        Delete
+        <v-icon>delete</v-icon>
+      </v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        searchText: "",
+      }
+    },
+    props: [
+      'routerNew',
+      'resultsText'
+    ],
+    methods: {
+      doFilter: function() {
+        this.$emit('doFilter', this.searchText)
+      },
+      doNew: function() {
+        this.$router.push({name: this.routerNew})
+      },
+      doDelete: function() {
+        console.log("doDelete")
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
